4551de5ce8917f55e6ae050fa5c62bb4bae7e361
José Juliano França <42650245+jjulianof@users.noreply.github.com>
2023-07-11 23:39:50 -0300

    primerias divs - menu e dashboard

---
+++ src/components/busca/busca.jsx
@@ -0,0 +1,9 @@
+function Busca() {
+  return (
+    <>
+      <input type="text" placeholder="Busca por negócios" />
+    </>
+  );
+}
+
+export default Busca;
---
+++ src/pages/dashoboard/dashboard.jsx
@@ -0,0 +1,22 @@
+import Busca from "../../components/busca/busca";
+import Menu from "../../components/menu/menu";
+function Dashboard() {
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row flex-nowrap">
+          <div className="col-auto col-md3 col-xl-2 px-sm-2 px0">
+            <Menu />
+          </div>
+
+          <div className="col py-3 me-3">
+            <Busca />
+            <h1>Dashboard....</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
